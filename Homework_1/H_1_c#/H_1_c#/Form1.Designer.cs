56bb9a50518da224cccb95ab6660259efc8e6112
Cheroberous <102479391+Cheroberous@users.noreply.github.com>
2023-10-08 17:24:39 +0200

    Aggiunto codice c#

---
+++ Homework_1/H_1_c#/H_1_c#/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace H_1_c_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "click to show names";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(421, 253);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(335, 170);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(53, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(335, 170);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(53, 253);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(335, 170);
+            this.panel3.TabIndex = 4;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Homework_1/H_1_c#/H_1_c#/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace H_1_c_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int c = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "gesu";
+            label3.Text = "That's a circle";
+            label2.Text = "That's a line";
+            label4.Text = "That's a Point";
+            label1.Text = "That's a Rectangle";
+
+
+
+            // for future use
+            //Graphics gra = this.panel3.CreateGraphics();
+            //Pen blackPen = new Pen(Color.Red, 3);
+            //PointF pnt1 = new PointF(50.0F, 50.0F);
+            //PointF pnt2 = new PointF(500.0F, 200.0F);
+            //gra.DrawLine(blackPen, pnt1, pnt2);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics gra = this.panel1.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            PointF pnt1 = new PointF(100.0F, 100.0F);
+            PointF pnt2 = new PointF(500.0F, 200.0F);
+
+            // e.Graphics.DrawLine(blackPen,pnt1, pnt2);
+            Rectangle r = new Rectangle(50, 50, 200, 50);
+            e.Graphics.DrawRectangle(blackPen,r);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gra = this.panel3.CreateGraphics();
+            
+            Pen blackPen = new Pen(Color.Red, 3);
+
+            PointF pnt1 = new PointF(50.0F, 50.0F);
+            PointF pnt2 = new PointF(500.0F, 200.0F);
+
+            
+
+            e.Graphics.DrawLine(blackPen, pnt1, pnt2);
+            
+           
+          
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+           
+            Pen blackPen = new Pen(Color.Blue, 3);
+            //                               x    y    h    w
+            e.Graphics.DrawEllipse(blackPen, 0, 0, 150, 150);
+
+           
+
+            SolidBrush brush = new SolidBrush(Color.Blue);
+
+            
+            e.Graphics.FillEllipse(brush,50,50,3,3);
+
+
+
+
+        }
+    }
+}
