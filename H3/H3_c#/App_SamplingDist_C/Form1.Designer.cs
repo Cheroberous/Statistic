46ea8b33477ecf3a6b94f869313be4b5c16e37a0
Cheroberous <102479391+Cheroberous@users.noreply.github.com>
2023-10-25 21:19:46 +0200

    Add files via upload

---
+++ H3/H3_c#/App_SamplingDist_C/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace App_SamplingDist_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            trackBar2 = new TrackBar();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            numericUpDown3 = new NumericUpDown();
+            label5 = new Label();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 38);
+            trackBar1.Maximum = 100;
+            trackBar1.Minimum = 10;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(104, 45);
+            trackBar1.TabIndex = 0;
+            trackBar1.Value = 10;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Sample size - 10";
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(163, 38);
+            trackBar2.Maximum = 1000;
+            trackBar2.Minimum = 100;
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(104, 45);
+            trackBar2.TabIndex = 2;
+            trackBar2.Value = 100;
+            trackBar2.Scroll += trackBar2_Scroll;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(163, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Number of samples - 100";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 89);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1875, 892);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(490, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 47);
+            button1.TabIndex = 5;
+            button1.Text = "Compute new distributions";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(331, 38);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(120, 23);
+            numericUpDown3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(348, 12);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Prob_breach";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(682, 14);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 47);
+            button2.TabIndex = 13;
+            button2.Text = "Optional_C_D";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1370, 749);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(numericUpDown3);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(trackBar2);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar trackBar1;
+        private Label label1;
+        private TrackBar trackBar2;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private NumericUpDown numericUpDown3;
+        private Label label5;
+        private Button button2;
+    }
+}
